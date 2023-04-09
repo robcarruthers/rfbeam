c87306a79d61c75e9753985bb09d3c41cf886055
Rob Carruthers <robc@hey.com>
2023-04-09 13:41:04 +1200

    Trying to get CI to pass

--- test/test_rfbeam.rb
+++ test/test_rfbeam.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class TestRfbeam < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Rfbeam::VERSION
+    refute_nil ::RfBeam::VERSION
   end
 
   def test_it_does_something_useful
