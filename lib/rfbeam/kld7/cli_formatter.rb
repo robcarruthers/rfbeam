d18a9c8036c716a440becd866f4ac697c2b454f7
Rob Carruthers <robc@hey.com>
2023-04-10 15:53:07 +1200

    Updated cli formatter

--- lib/rfbeam/kld7/cli_formatter.rb
+++ lib/rfbeam/kld7/cli_formatter.rb
@@ -10,41 +10,41 @@ module RfBeam
         when :tdat
           tdat(data)
         when :pdat
-          pdat_table(data)
+          pdat(data)
         when :ddat
           ddat(data)
         end
       end
-    end
 
-    def tdat(data)
-      { dist: data[2], speed: data[3], angle: data[4], mag: data[5] }
-    end
-
-    def pdat_table(data)
-      table = TTY::Table.new header: ['index', 'dist (M)', 'speed (Km/h)', 'angle (°)', 'mag (db)']
-      count = data[1] / 8
-      data.shift(2)
-      count.times do |index|
-        values = data.shift(4).map { |value| value.to_f / 100.0 }
-        table << [index, values].flatten
+      def tdat(data)
+        { dist: data[2], speed: data[3], angle: data[4], mag: data[5] }
       end
-      table
-    end
 
-    private
+      def pdat_table(data)
+        table = TTY::Table.new header: ['index', 'dist (M)', 'speed (Km/h)', 'angle (°)', 'mag (db)']
+        count = data[1] / 8
+        data.shift(2)
+        count.times do |index|
+          values = data.shift(4).map { |value| value.to_f / 100.0 }
+          table << [index, values].flatten
+        end
+        table
+      end
 
-    def ddat(data)
-      labels = ['Detection', 'Micro Detection', 'Angle', 'Direction', 'Range', 'Speed']
-      labels
-        .map
-        .with_index { |label, index| "#{label}: #{DETECTION_FLAGS[to_symbol(label)][data[index + 2]]}" }
-        .join("\n")
-    end
+      private
 
-    def to_symbol(string)
-      modified_string = string.gsub(' ', '_').downcase
-      modified_string.to_sym
+      def ddat(data)
+        labels = ['Detection', 'Micro Detection', 'Angle', 'Direction', 'Range', 'Speed']
+        labels
+          .map
+          .with_index { |label, index| "#{label}: #{DETECTION_FLAGS[to_symbol(label)][data[index + 2]]}" }
+          .join("\n")
+      end
+
+      def to_symbol(string)
+        modified_string = string.gsub(' ', '_').downcase
+        modified_string.to_sym
+      end
     end
   end
 end
